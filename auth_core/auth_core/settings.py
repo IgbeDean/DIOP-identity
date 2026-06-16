bd2e5bc02d37a6f07c963e7d1bd861472b9e5c0e
Igbe Dean <igbedean@gmail.com>
2026-06-16 15:22:49 +0100

    Made a few updates

--- auth_core/auth_core/settings.py
+++ auth_core/auth_core/settings.py
@@ -169,8 +169,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # 2. Gmail SMTP Server Settings
 EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
